1716b109cc43074136c1d5dd711874b7df0bf44c
mertcan <dikmenmertcan@gmail.com>
2025-06-04 21:39:56 +0300

    M1.0

---
+++ src/Mertcan.java
@@ -0,0 +1,6 @@
+public class Mertcan {
+    public static void main(String[] args) {
+
+        System.out.println("Kartal' lar yüksek uçar");
+    }
+}
--- src/ortak.java
+++ src/ortak.java
@@ -1,5 +1,7 @@
 public class ortak {
     public static void main(String[] args) {
         System.out.println("Merhaba Arkadaşlar");
+
+        System.out.println("Hi guys!");
     }
 }
