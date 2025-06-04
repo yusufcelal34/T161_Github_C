8ce4d4bb0c6e80e77d5f2f32d4be912163c2f532
Yusuf Celal <yusufcelal34@gmail.com>
2025-06-04 21:25:34 +0300

    first commit

---
+++ src/ortak.java
@@ -0,0 +1,5 @@
+public class ortak {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Arkadaşlar");
+    }
+}
---
+++ src/yusuf.java
@@ -0,0 +1,6 @@
+public class yusuf {
+    public static void main(String[] args) {
+
+        System.out.println("Yusuf");
+    }
+}
