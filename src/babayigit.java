a942e1547327f604d290c320c4ee47674e81b51f
babayigit <babayigit@gmail.com>
2025-06-04 21:32:20 +0300

    babayigit v1.0

---
+++ src/babayigit.java
@@ -0,0 +1,5 @@
+public class babayigit {
+    public static void main(String[] args) {
+        System.out.println("ne yaparsan yap AŞK ile yap");
+    }
+}
--- src/ortak.java
+++ src/ortak.java
@@ -1,5 +1,6 @@
 public class ortak {
     public static void main(String[] args) {
         System.out.println("Merhaba Arkadaşlar");
+        System.out.println("buradan babayigit geçti");
     }
 }
